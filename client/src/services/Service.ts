4c05bbf06c7ecd9539ea53ef74cbe45bf8cdeda1
Andreas Hammer <andreas.hammer.97@protonmail.com>
2020-06-21 17:09:28 +0200

    Fixed issue where API url when port = 80 (not part of origin) was parsed incorrectly

--- client/src/services/Service.ts
+++ client/src/services/Service.ts
@@ -4,6 +4,9 @@ export interface IGraphqlResponse<T> {
 
 export default class Service {
     protected getUrl(query: string): string {
+        if (window.location.origin.split(':').length == 2) {
+            return window.location.origin + ':3000/graphql?query=' + query;
+        }
         return window.location.origin.split(':').slice(0, -1).join(':') + ':3000/graphql?query=' + query;
     }
 }
